28483d173d5d3612404cf3602d9036b7fd089f9a
Sebastian Stammler <stammler@cbs.tu-darmstadt.de>
2017-08-31 17:00:15 +0200

    blockexplorer: refactor _api_request to return json() Switch ZEC api call to v2

--- blockexplorer.py
+++ blockexplorer.py
@@ -12,28 +12,26 @@
 import requests
 
 def _get_latest_BTC():
-    r = _api_request('https://blockchain.info/latestblock')
-    data = r.json()
+    data = _api_request_json('https://blockchain.info/latestblock')
     return data['height'], data['hash']
 
 def _get_latest_ETH():
-    r = _api_request('https://etherchain.org/api/blocks/count')
-    number = r.json()['data'][0]['count']
-    r = _api_request('https://etherchain.org/api/block/%i' % number)
-    _hash = r.json()['data'][0]['hash']
+    data = _api_request_json('https://etherchain.org/api/blocks/count')
+    number = data['data'][0]['count']
+    data = _api_request_json('https://etherchain.org/api/block/%i' % number)
+    _hash = data['data'][0]['hash']
     return number, _hash
 
 def _get_latest_ZEC():
-    r = _api_request('https://api.zcha.in/v1/mainnet/network')
-    data = r.json()
+    data = _api_request_json('https://api.zcha.in/v2/mainnet/network')
     return data['blockNumber'], data['blockHash']
 
-def _api_request(url):
+def _api_request_json(url):
     r = requests.get(url)
     if r.status_code != 200:
         raise Exception('API call %s returned status code %i' %\
                 url, r.status_code)
-    return r
+    return r.json()
 
 _getter = \
         {'BTC': _get_latest_BTC,
